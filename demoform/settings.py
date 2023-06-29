14827a2ebee15a5a173d0adde4ce9a474c30a0e1
Dat240906 <phungthanhdat001@gmail.com>
2023-06-29 10:53:33 +0700

    d

--- demoform/settings.py
+++ demoform/settings.py
@@ -38,9 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cloudinary',
-    'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic', 
 ]
 
 MIDDLEWARE = [
@@ -51,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'demoform.urls'
